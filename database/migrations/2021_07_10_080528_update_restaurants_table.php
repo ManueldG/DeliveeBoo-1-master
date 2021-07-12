8b32cb4eb8167297c1b75019a61e51a49e9bac29
AlfonsoBuononato <alfonso_buononato@hotmail.it>
2021-07-12 17:06:20 +0200

    esercizio

--- database/migrations/2021_07_10_080528_update_restaurants_table.php
+++ database/migrations/2021_07_10_080528_update_restaurants_table.php
@@ -20,7 +20,6 @@ class UpdateRestaurantsTable extends Migration
                   ->references('id')
                   ->on('users')
                   ->onDelete('set null');
-
         });
     }
 
