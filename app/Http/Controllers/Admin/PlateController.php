46e90a99869780b58225ffa843a7dd1dd9b4839f
MauroLaBarbera <you@example.com>
2021-07-13 12:36:30 +0200

    add: crud plate

---
+++ app/Http/Controllers/Admin/PlateController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Plate;
+use App\Restaurant;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PlateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // $restaurant = Restaurant::all();
+        // $plates = Plate::all();
+
+        // return view('admin.plates.index', compact('restaurant', 'plates'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $restaurants = Restaurant::all();
+        $plates = Plate::all();
+
+        return view('admin.plates.create', compact('plates','restaurants'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            // git
+            'price' => 'required',
+            'image' => 'nullable',
+        ],[
+            'required' => ' The :attribute is required.!!!!!!!',
+            'unique' => ' The :attribute is already taken',
+            'max' => 'Max :max characters allowed '
+        ]);
+
+        $data = $request->all();
+
+        $new_plate = new Plate();
+        $restaurant = Restaurant::all();
+
+        $new_plate->fill($data);
+
+        $new_plate->save();
+
+        return redirect()->route('admin.restaurants.show', $restaurant->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
