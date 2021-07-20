972b2794edc43ecc8766194dbd0be086333ee0fb
unknown <manuel.dellagala@gmail.com>
2021-07-20 12:30:04 +0200

    FIX_API

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Restaurant;
+use App\Plate;
 use Illuminate\Support\Facades\DB;
 
 class RestaurantController extends Controller
@@ -20,17 +21,21 @@ class RestaurantController extends Controller
 
 
         //$restaurant = Restaurant::where('name', $name)->with(['cuisines','plates'])->first();
-
-        $restaurant = DB::table('cuisine_restaurant')
+        $restaurants = DB::table('cuisine_restaurant')
             ->join('restaurants', 'restaurants.id', '=', 'cuisine_restaurant.restaurant_id')
             ->join('cuisines', 'cuisines.id', '=', 'cuisine_restaurant.cuisine_id')
-            ->select('*')
-            ->where('type','like',$type)
+            ->select('restaurants.id','restaurants.name','restaurants.description','restaurants.image','restaurants.address','restaurants.city','restaurants.cap','restaurants.phone_number','cuisines.type')
+            ->where('cuisines.type','like',$type)
             ->get();
+
+
+
+
 
         //$restaurant = Restaurant::where('name', $name)->with(['cuisines','plates'])->first();
 
-
-        return response()->json($restaurant);
+        return response()->json($restaurants);
+        //return response()->json($restaurants);
     }
 }
+
