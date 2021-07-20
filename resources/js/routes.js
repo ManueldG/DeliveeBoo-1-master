674a69f97eba60886536f1c0b01235f469d22474
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-07-20 12:11:37 +0200

    payment test

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -3,6 +3,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 
 import Home from "./pages/Home.vue";
+import Payment from "./pages/Payment.vue";
 import RestaurantDetail from "./pages/RestaurantDetail.vue";
 import NotFound from "./pages/NotFound.vue";
 
@@ -16,6 +17,11 @@ const router = new VueRouter({
             path: "/",
             name: "home",
             component: Home
+        },
+        {
+            path: "/payment",
+            name: "payment",
+            component: Payment
         },
         {
             path: "/restaurant/:name",
