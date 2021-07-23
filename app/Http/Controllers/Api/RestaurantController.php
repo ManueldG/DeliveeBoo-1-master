4f38ca960b98de9949f2e0b20551c03a4e323336
unknown <manuel.dellagala@gmail.com>
2021-07-23 12:35:53 +0200

    agg

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -30,18 +30,22 @@ class RestaurantController extends Controller
                 ->join('restaurants', 'restaurants.id', '=', 'cuisine_restaurant.restaurant_id')
                 ->join('cuisines', 'cuisines.id', '=', 'cuisine_restaurant.cuisine_id')
                 ->select('restaurants.id','restaurants.name','restaurants.description','restaurants.image','restaurants.address','restaurants.city','restaurants.cap','restaurants.phone_number')
-                ->distinct()
+
                 ->limit(20)
                 ->where('restaurants.id',$id)
                 ->get();
 
                 /*
-                SELECT A.restaurant_id AS Rist1, B.restaurant_id AS Rist2, C.cuisine_id AS Plate1, D.cuisine_id AS Plate2
-FROM plates A, plates B ,cuisine_restaurant C,cuisine_restaurant D
-WHERE A.restaurant_id = B.restaurant_id
-AND C.cuisine_id IN (1,7)
-AND C.cuisine_id <> D.cuisine_id
-ORDER BY `Plate1` ASC
+                SELECT A.restaurant_id AS Rist1, B.restaurant_id AS Rist2, A.cuisine_id AS Plate1, B.cuisine_id AS Plate2
+
+                FROM cuisine_restaurant A, cuisine_restaurant B
+                WHERE A.restaurant_id = B.restaurant_id
+
+                AND A.cuisine_id IN (1,7)
+                AND B.cuisine_id IN (1,7)
+                AND A.cuisine_id <> B.cuisine_id
+
+                ORDER BY `Plate1` ASC
                 */
 
 
@@ -66,16 +70,40 @@ ORDER BY `Plate1` ASC
 
                 $restaurants = DB::table('cuisine_restaurant')
                 ->join('restaurants', 'restaurants.id', '=', 'cuisine_restaurant.restaurant_id')
-                ->join('cuisines', 'cuisines.id', '=', 'cuisine_restaurant.cuisine_id')
-                ->select('restaurants.id','restaurants.name','restaurants.description','restaurants.image','restaurants.address','restaurants.city','restaurants.cap','restaurants.phone_number','cuisines.type')
-                ->limit(5)
-                ->where('cuisines.type',$str)
+                ->join('cuisine_restaurant AS c_res', 'c_res.id', '=', 'cuisine_restaurant.restaurant_id')
+                ->join('cuisines AS cucina1', 'cucina1.id', '=', 'cuisine_restaurant.cuisine_id')
+                ->join('cuisines AS cucina2', 'cucina2.id', '=', 'cuisine_restaurant.cuisine_id')
+                ->select('restaurants.*','cucina1.*','cucina2.*','c_res.*','cuisine_restaurant.*')
+
+                ->whereIn('cucina1.type',$str)
+                ->whereIn('cucina2.type',$str)
+                ->where('cuisine_restaurant.cuisine_id','<>','c_res.cuisine_id')
+
                 ->get();
 
-            //$restaurant = Restaurant::where('name', $name)->with(['cuisines','plates'])->first();
-            foreach($restaurants as $restaurant)
-            $restaurant->cuisine=$str;
+                /*
+                select A.restaurant_id AS Rist1, A.cuisine_id AS Plate1, B.cuisine_id AS Plate2
 
+                FROM cuisine_restaurant A, cuisine_restaurant B
+                WHERE A.restaurant_id = B.restaurant_id
+
+                AND A.cuisine_id IN (1,7)
+                AND B.cuisine_id IN (1,7)
+                AND A.cuisine_id <> B.cuisine_id
+				group by A.`restaurant_id`
+                ORDER BY `Plate1` ASC
+
+                $product1 = Product::find(1);
+$children = Product::where('id', '<>', $product1->id)->
+    where('color_id', $product1->color_id)->
+    get();
+                */
+
+            //$restaurant = Restaurant::where('name', $name)->with(['cuisines','plates'])->first();
+             /*foreach($restaurants as $restaurant)
+
+            $restaurant->cuisine=$str;
+*/
             return response()->json(['success' => true,
             'results' => $restaurants]);
             //return response()->json($restaurants);
